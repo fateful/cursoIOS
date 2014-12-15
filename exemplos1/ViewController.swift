eecee0d70278793f28553d107980b1fd15926f87
Devios <devios@Popcodes-MacBook-Pro.local>
2014-12-15 20:07:00 -0300

    inicio

--- exemplos1/ViewController.swift
+++ exemplos1/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var saudacaoEditText: UITextField!
+    
+    @IBOutlet weak var nomeEditText: UITextField!
+    
+    @IBOutlet weak var resultadoEditText: UILabel!
+    
+    @IBOutlet weak var textoBotao: UIButton!
+    @IBAction func clickMudanca(sender: UIButton) {
+        
+        resultadoEditText.hidden = false
+        resultadoEditText.text = saudacaoEditText.text + " " + nomeEditText.text
+        nomeEditText.text = nil
+        saudacaoEditText.text = nil
+        
+        saudacaoEditText.resignFirstResponder()
+        nomeEditText.resignFirstResponder()
+
+        resultadoEditText.textColor = UIColor.redColor()
+        textoBotao.setTitle("Opa", forState:UIControlState.Normal )
+        textoBotao.setTitle("Pressionado", forState:UIControlState.Highlighted )
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +44,6 @@ class ViewController: UIViewController {
     }
 
 
+    
 }
 
